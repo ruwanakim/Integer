b5131e5192254d25c431a5f6d178ea32f633d258
ruhshona <ruwanakim@gmail.com>
2022-02-04 11:40:19 +0500

    zadachi

---
+++ index.js
@@ -0,0 +1,315 @@
+///////                ЦЕЛЫЕ ЧИСЛА           ///////
+
+
+// ///////             Задача №1
+// let L = Number(prompt('L'));
+// metr = L/100;
+// alert('metr = ' + metr);
+
+
+
+
+// ///////                 Задача №2
+// let M = Number(prompt('M'));
+// tonn = M/1000;
+// alert('tonn = ' + tonn);
+
+
+
+
+
+
+// ///////                   Задача №3
+// let B = Number(prompt('KB'));
+// KB = B/1024;
+// alert('KB = ' + KB);
+
+
+
+
+
+
+// ///////                   Задача №4
+// let A = Number(prompt('A'));
+// let B = Number(prompt('B'));
+// x = A/B;
+// alert('x = ' + x);
+
+
+
+
+
+// ///////                    Задача №5
+// let A = Number(prompt('A'));
+// let B = Number(prompt('B'));
+// x = A % B;
+// alert('x = ' + x);
+
+
+
+
+
+// ///////                     Задача №6
+// let A = Number(prompt('A'));
+// let x = parseInt(A / 10);
+// let y = A % 10;
+// alert('x = ' + x);
+// alert('y = ' + y);
+
+
+
+
+
+// ///////                        Задача №7
+// let A = Number(prompt('A'));
+// let x = parseInt( A/10 ) + ( A%10 );
+// let y = parseInt( A/10 ) * ( A%10 );
+// alert('x = ' + x);
+// alert('y = ' + y);
+
+
+
+
+// ///////                      Задача №8
+// let A = Number(prompt('A'));
+// x = ( A / 10 ) + ( A % 10 );
+// let y = (A/10 * A % 10);
+// alert('x = ' + x);
+// alert('y = ' + y);
+
+
+
+
+// ///////                      Задача №9
+// let A = Number(prompt('A'));
+// x = A / 100;
+// alert('x = ' + x);
+
+
+
+
+// ///////                       Задача №10
+// let A = Number(prompt('A'));
+// A = ( A / 100 ) * 10 + ( A / 100 ) / 10;
+// alert('A = ' + A);
+
+
+
+
+
+// ///////                        Задача №11
+// let A = Number(prompt('A'));
+// let x = parseInt( A/100 ) + ( A%100 );
+// let y = parseInt( A/100 ) * ( A%100 );
+// alert('x = ' + x);
+// alert('y = ' + y);
+
+
+
+
+
+// ///////                         Задача №12
+// let A = Number(prompt('A'));
+// let x = parseInt( A / 100 );
+// let y = parseInt( A % 100 / 10 );
+// let z = ( A % 100 ) % ( y * 10);
+// alert(""+ z + y + x);
+
+
+
+
+
+// ///////                          Задача №13
+// let A = Number(prompt('A'));
+// let x = parseInt( A / 100 );
+// let y = parseInt( A % 100 / 10 );
+// let z = ( A % 100 ) % ( y * 10);
+// alert(""+ y + z + x);
+
+
+
+
+
+
+// ///////                           Задача №14
+// let A = Number(prompt('A'));
+// let x = parseInt( A / 100 );
+// let y = parseInt( A % 100 / 10 );
+// let z = ( A % 100 ) % ( y * 10);
+// alert(""+ z + x + y);
+
+
+
+
+
+
+// ///////                              Задача №15
+// let A = Number(prompt('A'));
+// let x = parseInt( A / 100 );
+// let y = parseInt( A % 100 / 10 );
+// let z = ( A % 100 ) % ( y * 10);
+// alert(""+ y + x + z);
+
+
+
+
+
+
+// ///////                               Задача №16
+// let A = Number(prompt('A'));
+// let x = parseInt( A / 100 );
+// let y = parseInt( A % 100 / 10 );
+// let z = ( A % 100 ) % ( y * 10);
+// alert(""+ x + z + y);
+
+
+
+
+
+
+// ///////                               Задача №17
+// let A = Number(prompt('A'));
+// let x = parseInt((A % 100) / 100 * 10);
+// alert('x = ' + x);
+
+
+
+
+
+
+// ///////                             Задача №18
+// let A = Number(prompt('A'));
+// let x = parseInt(A / 100);
+// let y = x % 10;
+// alert('y = ' + y);
+
+
+
+
+
+
+// ///////                            Задача №19
+// let A = Number(prompt('A'));
+// let x = parseInt(A / 100);
+// let y = parseInt(x / 10);
+// alert('y = ' + y);
+
+
+
+
+
+
+// ///////                             Задача №20
+// let A = Number(prompt('A'));
+// let x = A / 60;
+// let y = parseInt( x / 60);
+// alert('y = ' + y);
+
+
+
+
+
+
+// ///////                            Задача №21
+// let A = Number(prompt('A'));
+// let x = A / 60;
+// alert('x = ' + x);
+
+
+
+
+
+
+// ///////                             Задача №22
+// let A = Number(prompt('A'));
+// let x = A / 60;
+// let y = parseInt( x / 60);
+// alert('x = ' + x);
+// alert('y = ' + y);
+
+
+
+
+
+
+// ///////                              Задача №23
+// let A = Number(prompt('A'));
+// let x = A % ( 60 * 60 );
+// let y = x % 60;
+// alert('x = ' + x);
+// alert('y = ' + y);
+
+
+
+
+
+
+// ///////                            Задача №24
+// let K = Number(prompt('K'));
+// let x = K % 7;
+// alert('x = ' + x);
+
+
+
+
+
+
+// ///////                           Задача №25
+// let K = Number(prompt('K'));
+// let x = ( K + 3 ) % 7;
+// alert('x  = ' + x);
+
+
+
+
+
+
+// ///////                         Задача №26
+// let K = Number(prompt('K'));
+// let x = ( K % 7 ) + 1;
+// alert('x  =' + x);
+
+
+
+
+
+
+// ///////                         Задача №27
+// let K = Number(prompt('K'));
+// let x = ( K + 4) % 7) + 1;
+// alert('x = ' + x);
+
+
+
+
+
+
+// ///////                        Задача №28
+// let K = Number(prompt('K'));
+// let N = Number(prompt('N'));
+// let x = (( K + N - 2) % 7) + 1;
+// alert('x = ' + x);
+
+
+
+
+
+
+// ///////                          Задача №29
+// let A = Number(prompt('A'));
+// let B = Number(prompt('B'));
+// let C = Number(prompt('C'));
+// let x = (A / C) * (B / C);
+// let y =A * B - x ** 2 * (C);
+// alert('x = ' + x);
+// alert('y = ' + y);
+
+
+
+
+
+
+// ///////                           Задача №30
+// let A = Number(prompt('A'));
+// let B = (( A - 1 ) / 100 ) + 1;
+// alert('B = ' + B);
